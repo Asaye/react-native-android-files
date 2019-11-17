42a8094452db2af642b348c73633d9d7e95741ca
Asaye <asayechemeda@yahoo.com>
2019-11-17 19:47:20 +0300

    finalized

---
+++ README.md
@@ -0,0 +1,228 @@
+# react-native-android-files
+
+<p style = "text-align: justify">A simple react-native module which is used to pick, open, upload and download files.
+
+# Getting Started
+
+### Installation
+
+Using npm
+
+``` $ npm install react-native-android-files --save```
+
+Using yarn
+
+``` $ yarn add react-native-android-files```
+
+### Linking
+There are two options for linking:
+
+##### 1. Automatic
+
+``` react-native link react-native-android-files```
+
+##### 2. Manual
+
+If the automatic linking fails for some reason, you can do the linking manually as follows:
+ * add the following to <code>yourAppName/android/settings.gradle</code> file:
+ 
+ ```
+  include ':react-native-android-files'
+  project(':react-native-android-files').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-android-files/android')
+ ```
+
+ * add the following inside the dependencies closure of  <code>yourAppName/android/app/build.gradle</code> file:
+ ```
+  implementation project(':react-native-android-files')
+```
+
+* add the following to your <code>MainApplication.java</code> file:
+ ```
+  import com.adfiles.files.FilesPackage;
+ ```
+ and also,
+ ```
+  @Override
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+      new MainReactPackage(),
+      ....
+      new FilesPackage()                       <== Add this
+    );
+  }
+ ```
+
+
+### Usage
+To perform activities on with the module, first import the it to your app like so:
+
+```   import { Files } from 'react-native-android-files';```
+
+After this, you can call any of the functions stated below as appropriate.
+
+## Functions
+
+<p style = "text-align: justify">The files module contains the following functions.</p>
+
+``` 
+        open(String path)
+        getPath()
+        pick()
+        download(String src, String dest)
+        upload(String dest)
+```
+
+### Permissions
+
+No permission is required open a file picker dialog with <code>getPath()</code> function and to obtain the path of the selected file as a response. However, to download a file and to save it on the device with <code>download()</code> function, only the first configuration, i.e., adding the storage permission,  is required. In addition, to open a file for a given path from the device with <code>open()</code> function or to pick a file from a dialog and to open it via <code>pick()</code> function, the following  configurations should be made.
+For API level 23 and below devices, only the first configuration is required. For API level 24 and above, all the three configurations are mandatory. The third configuration can be changed according to official android [documentation](https://developer.android.com/reference/android/support/v4/content/FileProvider) regarding <code>FileProvider</code> class.
+
+1. Add the following permission outside the application tage of the AndroidManifest.xml file.
+
+ ```    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/> ```
+
+2. Add the following provider tag within the application tag in the AndroidManifest.xml file.</p>
+
+  ```
+        <provider
+                 android:name="android.support.v4.content.FileProvider"
+                 android:authorities="${applicationId}"
+                 android:exported="false"
+                 android:grantUriPermissions="true">
+                 <meta-data
+                     android:name="android.support.FILE_PROVIDER_PATHS"
+                     android:resource="@xml/provider_paths">
+                 </meta-data>
+         </provider>
+```
+
+3. Create a folder named <code>xml</code> inside <code>yourAppName\android\app\src\main\res</code> (if there isn't any) and create a file named <code>provider_paths.xml</code> inside the folder and add the following content in the file. As stated above, the contents of this file can be changed to meet your desired accessible file locations.
+
+```
+    <?xml version='1.0' encoding='utf-8'?>
+        <paths xmlns:android="http://schemas.android.com/apk/res/android">
+            <root-path name="root" path=""/>
+            <files-path name="files" path="/" />
+            <external-files-path name="external_files" path="" />
+            <external-path name="external" path="." />
+            <cache-path name="cache" path="/" />
+        </paths>
+```
+
+### Description
+<p style = "text-align: justify">The above functions are used to perform the following activities.</p>
+
+#### &#x1F537; open(String path): 
+
+<p style = "text-align: justify">is used to open a file from a location specified by the <code>path</code> parameter. A number of common file types will be opened by calling this function via the default app on the device. Note that the device should have an installed app to open the given file type. For instance, if you want to open a pdf file, the file will be opened successfully only if there is a pdf viewer installed on the device.</p>
+
+##### Sample code snippet
+
+``` 
+            import { Files } from 'react-native-android-files';
+            ....
+            ....
+
+            _openFile = () => {
+                const path = "/storage/emulated/0/DCIM/Camera/myPic.jpg";
+                Files.open(path).catch((err) => {
+                    console.log("error");
+                });
+            } 
+```
+<p style = "text-align: justify">Call to <code>_openFile()</code> will open the specified file for successful requests or a promise rejection will be sent if something goes wrong.</p>
+
+#### &#x1F537; getPath(): 
+
+<p style = "text-align: justify">is used to get a path of a file where a file picker dialog will be opened and the user picks any file from the device. For successful requests, the path of the selected file will be sent as a response or a promise rejection otherwise. </p>
+
+##### Sample code snippet
+
+``` 
+            import { Files } from 'react-native-android-files';
+            ....
+            ....
+
+            _getFilePath = () => {
+                Files.getPath().then((path) => {
+                    console.log(path);
+                }).catch((err) => {
+                    console.log(err);
+                });
+            } 
+```
+
+##### Sample result 
+<p style = "text-align: justify">Call to  <code>_getFilePath()</code> function opens a file picker dialog, waits for a user to select a file and logs the path of the selected file. A sample response may look like the following:
+
+```
+                "file:///storage/emulated/0/Download/myfile.mp4"
+```
+
+#### &#x1F537; pick(): 
+
+<p style = "text-align: justify">is used to open  file picker dialog so that the user can pick and open a file from the device. Note that, for API level 24 and above, in order to successfully open the desired file, the correct configuration should be made to grant access to the desired file location as described above. </p>
+
+##### Sample code snippet
+
+``` 
+            import { Files } from 'react-native-android-files';
+            ....
+            ....
+
+            _openFileFromPicker = () => {
+                 Files.pick().catch((err) => {
+                    console.log(err);
+                 });
+            } 
+```
+
+
+<p style = "text-align: justify">Call to  <code>_openFileFromPicker()</code> function opens a file picker dialog, waits for a user to select a file and opens the file if access is rightfully granted.</p>
+
+#### &#x1F537; download(String src, String dest): 
+
+<p style = "text-align: justify">is used to download file from <code>src</code> to <code>dest</code>. The <code>src</code> parameter can be either a file location on the device or a webservice. If it is the former one, the actual file location should be preceded by a protocol, i.e., <code>file://</code>. The <code>dest</code> parameter can be either a presumed complete file path or a directory where the downloaded file will be stored. You use the former one if you know the file mime type before downloading and you want to give your own custom name to the file. Where as the latter is used if you want the downloaded file to take the same name as it has in the source location.</p>
+
+##### Sample code snippet
+
+``` 
+            import { Files } from 'react-native-android-files';
+            ....
+            ....
+
+            _downloadFile = () => {
+                const src = "http://www.mywebservice.com/download/myVideo.mp4",
+                      dest = "/storage/emulated/0/videos/";
+                Files.download(src, dest).catch((err) => {
+                    console.log(err);
+                });
+            } 
+```
+
+<p style = "text-align: justify">Call to  <code>_downloadFile()</code> function downloads a file from the webservice specified by <code>src</code> variable and saves it in a directory specified by <code>dest</code> variable.</p>
+
+#### &#x1F537; upload(String dest): 
+
+<p style = "text-align: justify">is used to upload a file picked from the device on to a webservice specified by <code>dest</code> parameter. Note that, for API level 24 and above, in order to successfully upload the desired file, the correct configuration should be made to grant access to the desired file location as described above. </p>
+
+##### Sample code snippet
+
+``` 
+            _uploadFile = () => {
+                const dest = "http://www.mywebservice.com/upload";
+                Files.upload(dest).catch((err) => {
+                    console.log(err);
+                });
+            } 
+```
+
+##### Sample result 
+<p style = "text-align: justify">Call to  <code>_uploadFile()</code> function opens a file picker dialog, waits for a user to select a file and uploads the selected file to a webservice specified by <code>dest</code> parameter if access is rightfully granted.</p>
+
+
+## Issues or suggestions?
+If you have any issues or if you want to suggest something , you can write it [here](https://github.com/Asaye/react-native-android-files/issues).
+
+## Additional info
+This is a component of a more comprehensive [react-native-system-applications](https://www.npmjs.com/package/react-native-system-applications) module developed by the same author.
---
+++ android/src/main/java/com/adfiles/files/Files.java
@@ -0,0 +1,356 @@
+package com.adfiles.files;
+
+import com.adfiles.utils.Messages;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.modules.core.PermissionAwareActivity;
+import com.facebook.react.modules.core.PermissionListener;
+
+import android.net.Uri;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.content.FileProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Build;
+import android.os.Process;
+import android.os.Handler;
+import android.os.HandlerThread;
+
+import java.lang.Exception;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.io.File;
+
+public class Files extends ReactContextBaseJavaModule 
+            implements PermissionListener {
+
+    private static final String TAG = "FILE_OPEN_ERROR";
+    private static final int FILE_PICK_REQUEST = 10050;
+    private static final int FILE_OPEN_REQUEST = 10051;
+    private static final int FILE_TRANSFER_REQUEST = 10052;
+    private static final int FILE_UPLOAD_REQUEST = 10053;
+    private static final int PERMISSIONS_REQUEST_OPEN = 10054;
+    private static final int PERMISSIONS_REQUEST_PICK = 10055;
+    private static final int PERMISSIONS_REQUEST_DOWNLOAD = 10056;
+    private static final int PERMISSIONS_REQUEST_UPLOAD = 10057;
+    
+
+    private static final String[] PERMISSIONS = 
+                        new String[] {   
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        };
+    
+    private Promise mFilePickPromise;
+    private Promise mFileOpenPromise;
+    private Promise mFileTransferPromise;
+    private Promise mFileUploadPromise;
+    private Uri mFileUri;
+    private String mUrl;
+    private String mSource;
+    private String mDestination;
+
+    private final ActivityEventListener filesEventListener = new BaseActivityEventListener() {
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+
+            if (mFilePickPromise != null && requestCode == FILE_PICK_REQUEST) {
+                if (resultCode == Activity.RESULT_OK) {                    
+                    Uri uri = intent.getData();
+
+                    if (uri == null) {
+                        mFilePickPromise.reject(TAG, "No file data found");
+                    } else {
+                        String path = URLDecoder.decode(uri.toString());
+                        mFilePickPromise.resolve(path);
+                    }
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    mFilePickPromise.reject(TAG, Messages.PICKER_CANCELLED);
+                }
+                mFilePickPromise = null;
+            } else if (mFileOpenPromise != null && requestCode == FILE_OPEN_REQUEST) {
+
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri uri = intent.getData();
+                    if (uri != null) {
+                        mUrl = uri.getPath();
+                        getUri();
+                    }                    
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    mFileOpenPromise.reject(TAG, Messages.PICKER_CANCELLED);
+                }
+                mFileOpenPromise = null;
+            } else if (requestCode == FILE_UPLOAD_REQUEST) {
+
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri uri = intent.getData();
+
+                    if (uri != null) {
+                        String path = uri.getPath();
+                        File file = new File(path);
+
+                        if (!file.isFile() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            try {
+                                Context context = getReactApplicationContext();
+                                String packageName = context.getPackageName();
+                                Uri fpUri = FileProvider.getUriForFile(getCurrentActivity(), packageName, file);
+                                context.grantUriPermission(packageName, fpUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                path = URLDecoder.decode(fpUri.toString()); 
+                                file = new File(path);
+                                if (!file.isFile()) {
+                                    mFileUploadPromise.reject(TAG, Messages.FILE_INACCESSIBLE);
+                                }
+                            } catch (Exception ex) {
+                                mFileUploadPromise.reject(TAG, ex.getMessage());
+                            }
+                        }                        
+                        HandlerThread thread = new HandlerThread("FileUpload");
+                        thread.start();
+                        Handler handler = new Handler(thread.getLooper());     
+                        handler.post(new Uploader(file, mDestination, mFileUploadPromise));
+                    }                  
+                } else if (mFileUploadPromise != null && resultCode == Activity.RESULT_CANCELED) {
+                    mFileUploadPromise.reject(TAG, "User has cancelled the file picker");
+                }
+                mFileUploadPromise = null;
+            } else if (mFileTransferPromise != null && requestCode == FILE_TRANSFER_REQUEST) {
+
+                if (resultCode == Activity.RESULT_OK) {
+                    downloadFile();                   
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    mFileTransferPromise.reject(TAG, Messages.PERMISSION_DENIED);
+                }
+                mFileTransferPromise = null;
+            } 
+        }
+    };
+
+    private LifecycleEventListener lifeEventListener = new LifecycleEventListener() {
+        public void onHostResume() { 
+        }
+
+        public void onHostPause() { 
+        }
+
+        public void onHostDestroy() { 
+            if (mFileUri == null) return;
+            Context context = getReactApplicationContext();
+            if (context == null) return;
+            String packageName = context.getPackageName();
+            context.grantUriPermission(packageName, mFileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION 
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+    };
+    
+    public Files(ReactApplicationContext reactContext) {
+        super(reactContext);
+        reactContext.addActivityEventListener(filesEventListener);
+        reactContext.addLifecycleEventListener(lifeEventListener);
+    }
+
+    @Override
+    public String getName() {
+        return "Files";
+    }
+
+    @Override
+    public boolean onRequestPermissionsResult(
+        int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults.length == PERMISSIONS.length) {
+            if (requestCode == PERMISSIONS_REQUEST_OPEN) {
+                getUri();
+            } else if (requestCode == PERMISSIONS_REQUEST_PICK) {
+                openDialog(FILE_OPEN_REQUEST);
+            } else if (requestCode == PERMISSIONS_REQUEST_DOWNLOAD) {
+                downloadFile();
+            } else if (requestCode == PERMISSIONS_REQUEST_UPLOAD) {
+                openDialog(FILE_UPLOAD_REQUEST);
+            }            
+        } else {
+            mFileOpenPromise.reject(TAG, Messages.PERMISSION_DENIED);
+        }
+        return true;
+    } 
+
+    @ReactMethod
+    public void open(String url, Promise promise) {
+        mUrl = url;
+        mFileOpenPromise = promise;
+        if (checkPermission()) {
+            getUri();
+        } else {
+            requestPermissions(PERMISSIONS_REQUEST_OPEN); 
+        }
+    }
+
+    @ReactMethod
+    public void pick(Promise promise) {  
+        mFileOpenPromise = promise;
+        if (checkPermission()) {
+            openDialog(FILE_OPEN_REQUEST);
+        } else {
+            requestPermissions(PERMISSIONS_REQUEST_PICK); 
+        }            
+    }
+
+    @ReactMethod
+    public void getPath(Promise promise) {
+        mFilePickPromise = promise;
+        openDialog(FILE_PICK_REQUEST);        
+    }
+
+    @ReactMethod
+    public void download(String src, String dest, Promise promise) { 
+        mSource = src;
+        mDestination = dest; 
+        mFileTransferPromise = promise;
+        if (checkPermission()) {
+            downloadFile();
+        } else {
+            requestPermissions(PERMISSIONS_REQUEST_DOWNLOAD); 
+        }
+    } 
+
+    @ReactMethod
+    public void upload(String dest, Promise promise) {
+        mDestination = dest; 
+        mFileUploadPromise = promise;
+        if (checkPermission()) {
+            openDialog(FILE_UPLOAD_REQUEST);
+        } else {
+            requestPermissions(PERMISSIONS_REQUEST_UPLOAD); 
+        }        
+    }   
+
+    private boolean checkPermission() {        
+        int pid = Process.myPid();
+        int uid = Process.myUid();
+        int status_ok = PackageManager.PERMISSION_GRANTED;        
+        Context context = getReactApplicationContext().getBaseContext();
+
+        for (String permission : PERMISSIONS) {
+            if (context.checkPermission(permission, pid, uid) != status_ok) {
+                return false;
+            }
+        }
+
+        return true;   
+    }
+
+    private void requestPermissions(int requestCode) {
+        Activity activity = getCurrentActivity();
+        if (activity == null) return;
+        ((PermissionAwareActivity) activity)
+                        .requestPermissions(PERMISSIONS,requestCode, this); 
+    }
+
+    private void openDialog(int requestCode) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        getCurrentActivity().startActivityForResult(intent, requestCode);
+    }
+
+    private void getUri() {
+        try {  
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                openProvidedFile();                
+            } else { 
+                openFile();
+            }
+            
+        } catch (Exception ex) {
+            if (mFileOpenPromise != null) {
+                mFileOpenPromise.reject(TAG, ex.getMessage());
+            }
+        }
+    }
+
+    private void downloadFile() {
+        HandlerThread thread = new HandlerThread("FileDownload");
+        thread.start();
+        Handler handler = new Handler(thread.getLooper());                        
+        handler.post(new Downloader(mSource, mDestination, mFileTransferPromise));
+    }
+    
+    private void openProvidedFile() {
+        Activity activity = getCurrentActivity();
+        if (activity == null) return;
+
+        PackageManager pm = activity.getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        File file = new File(mUrl);
+        Context context = getReactApplicationContext();
+        String packageName = context.getPackageName();
+        mFileUri = FileProvider.getUriForFile(activity, packageName, file); 
+        String mimeType = null;
+
+        context.grantUriPermission(packageName, mFileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | 
+                                            Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | 
+                         Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        mimeType = context.getContentResolver().getType(mFileUri);
+        intent.setDataAndType(mFileUri, mimeType);
+        
+        if (intent.resolveActivity(pm) != null) {
+            activity.startActivity(intent);
+        }
+    }
+
+    private void openFile() {
+
+        Activity activity = getCurrentActivity();
+
+        if (activity == null) return;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        File file = new File(mUrl);
+        Uri uri = Uri.fromFile(file);
+        String url = uri.toString();
+                
+        if (url.toString().contains(".doc") || url.toString().contains(".docx")) {
+            intent.setDataAndType(uri, "application/msword");
+        } else if(url.toString().contains(".pdf")) {
+            intent.setDataAndType(uri, "application/pdf");
+        } else if(url.toString().contains(".ppt") || url.toString().contains(".pptx")) {
+            intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        } else if(url.toString().contains(".xls") || url.toString().contains(".xlsx")) {
+            intent.setDataAndType(uri, "application/vnd.ms-excel");
+        } else if(url.toString().contains(".zip") || url.toString().contains(".rar")) {
+            intent.setDataAndType(uri, "application/x-wav");
+        } else if(url.toString().contains(".rtf")) {
+            intent.setDataAndType(uri, "application/rtf");
+        } else if(url.toString().contains(".wav") || url.toString().contains(".mp3")) {
+            intent.setDataAndType(uri, "audio/x-wav");
+        } else if(url.toString().contains(".gif")) {
+            intent.setDataAndType(uri, "image/gif");
+        } else if(url.toString().contains(".jpg") || url.toString().contains(".jpeg") || 
+                  url.toString().contains(".png")) {
+            intent.setDataAndType(uri, "image/jpeg");
+        } else if(url.toString().contains(".txt")) {
+            intent.setDataAndType(uri, "text/plain");
+        } else if(url.toString().contains(".3gp") || url.toString().contains(".mpg") || 
+            url.toString().contains(".mpeg") || url.toString().contains(".mpe") || 
+            url.toString().contains(".mp4") || url.toString().contains(".avi")) {
+            intent.setDataAndType(uri, "video/*");
+        } else {
+            intent.setDataAndType(uri, "*/*");
+        }
+
+        PackageManager pm = activity.getPackageManager();
+        if (intent.resolveActivity(pm) != null) {
+            activity.startActivity(intent);
+        }
+    }
+}
